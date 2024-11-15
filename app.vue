37471f94ee00c821816c61a42f495fb2b6b49434
naruko_hstk <34113239+naruko-hstk@users.noreply.github.com>
2024-11-16 03:16:36 +0800

    init

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <NuxtLayout :name="layout"><NuxtPage /></NuxtLayout>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+const layout = route.path.startsWith("dashboard", 1) ? "dashboard" : "default"
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+import { AppHeader, UContainer, AppFooter } from '../.nuxt/components';
+<template>
+  <div>
+    <AppHeader />
+    <UContainer>
+      <main>
+        <slot />
+      </main>
+    </UContainer>
+    <AppFooter />
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
